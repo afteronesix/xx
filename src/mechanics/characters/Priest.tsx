a69b4ed6011357dd5622979d0c955e1f9a873abf
Shot <74735269+afteronesix@users.noreply.github.com>
2025-11-10 20:39:43 +0000

    feat: implement character classes with skills and damage mechanics

---
+++ src/mechanics/characters/Priest.tsx
@@ -0,0 +1,12 @@
+import Character, { ICharacterStats } from "./Character";
+
+export class Mage extends Character {
+    constructor(characterStats: ICharacterStats) {
+        super(characterStats)
+    }
+
+    healTouch(dmgToCharacter: Character) {
+        dmgToCharacter.setHp(dmgToCharacter.getHp() + 12)
+    }
+
+}
